bc50586bb0ce5ae29486774bfe8285cea9a9f0ca
Sapphyruss <lugi.franklin@outlook.fr>
2024-03-04 13:44:28 -0800

    .

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Use let or const here
-    let task2 = false; // Use let or const here
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
 }
-

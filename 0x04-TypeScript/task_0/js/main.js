2535404b6f05812de59b75ecf17214bcee32526b
Sapphyruss <lugi.franklin@outlook.fr>
2024-03-15 04:37:58 -0700

    .

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,14 @@
+var student1 = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 17,
+    location: 'Bwari',
+};
+var student2 = {
+    firstName: 'John',
+    lastName: 'Bull',
+    age: 17,
+    location: 'Gwagwa',
+};
+var studentsList = [student1, student2];
+console.log(studentsList);
